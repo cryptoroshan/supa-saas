b6018f82581eab16fdd0e8c420e8ff5fc3e415f1
tinyphoniex <royalguy1031@gmail.com>
2023-11-13 00:40:24 +0900

    fixed some ui bugs

---
+++ components/AppFooter.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="sticky z-50 bottom-0 p-4 bg-base-200">
+    <NuxtLink to="/terms">Terms Of Service</NuxtLink>
+    <span class="px-2">|</span>
+    <NuxtLink to="/privacy">Privacy</NuxtLink>
+    <span class="px-2">|</span>
+    <button type="button" data-cc="c-settings">Cookie settings</button>
+  </div>
+</template>
